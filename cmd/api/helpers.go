0532ad8eba0d2dd0d596eb18481fa8ff1da8f38f
Adan M <adan@fractalframing.com>
2023-03-08 23:51:32 -0600

    ch3.5

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -18,7 +18,9 @@ func (app *application) readIDParam(r *http.Request) (int64, error) {
 	return id, nil
 }
 
-func (app *application) writeJSON(w http.ResponseWriter, status int, data any, headers http.Header) error {
+type envelope map[string]any
+
+func (app *application) writeJSON(w http.ResponseWriter, status int, data envelope, headers http.Header) error {
 	res, err := json.Marshal(data)
 	if err != nil {
 		return err
